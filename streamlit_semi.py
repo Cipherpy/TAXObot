fa4a2592f9f508c58fea7e6dcbc2de0227a884d9
Cipherpy <reshmababuraj89@gmail.com>
2026-01-13 15:38:03 +0530

    requirements.txt

--- streamlit_semi.py
+++ streamlit_semi.py
@@ -183,9 +183,9 @@ with col1:
                 <span style="font-size: 1.2em;">⚠️</span>
                 <strong style="margin-left: 6px;">Disclaimer</strong><br><br>
 
-                TAXObot does not provide exhaustive coverage of all marine species.<br>
-                The current version is limited to a selected subset of species within the listed taxonomic groups.<br>
-                This application is a prototype developed for research and evaluation purposes.<br>
+                TAXObot does not provide exhaustive coverage of all marine species
+                The current version is limited to a selected subset of species within the listed taxonomic groups
+                This application is a prototype developed for research and evaluation purposes
                 Additional species, taxonomic groups, and enhanced functionalities will be incorporated in future releases.
             </p>
         </div>
